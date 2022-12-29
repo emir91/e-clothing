85a125d897aca37a5213274fd77bf4383b627c42
Emir Vranac <emir.vranac@symphony.is>
2022-12-29 11:56:25 +0100

    Add logic for creating User doc inside firebase db

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, signinWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
 // My web app's Firebase configuration
 const firebaseConfig = {
@@ -11,14 +12,44 @@ const firebaseConfig = {
     appId: "1:38454442610:web:a2f720eb9ec93e5171e0eb"
   };
   
-  // Initialize Firebase
-  const firebaseApp = initializeApp(firebaseConfig);
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
 
-  const provider = new GoogleAuthProvider()
+const provider = new GoogleAuthProvider()
 
-  provider.setCustomParameters({
-    prompt: 'select_account'
-  })
+provider.setCustomParameters({
+prompt: 'select_account'
+})
 
-  export const auth = getAuth()
-  export const signinWithGooglePopup = () => signInWithPopup(auth, provider) 
+export const auth = getAuth()
+export const signinWithGooglePopup = () => signInWithPopup(auth, provider) 
+
+const db = getFirestore()
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid)
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef)
+
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+    if(!userSnapshot.exists()) {
+        const { displayName, email } = userAuth
+        const createdAt = new Date()
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            })
+        } catch (error) {
+            console.log('Error creating the user', error.message);
+        }
+
+        return userDocRef;
+    }
+}
