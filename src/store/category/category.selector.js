f4aceb77ae8091999775fb3ff0824dd26b83e341
Emir Vranac <emir.vranac@symphony.is>
2023-03-14 14:44:00 +0100

    Add reselect in order to cache category state

--- src/store/category/category.selector.js
+++ src/store/category/category.selector.js
@@ -1,5 +1,17 @@
-export const categoriesSelector = (state) => state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-}, {});;
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (categorySlice) => categorySlice.categories
+);
+
+export const categoriesSelector = createSelector(
+    [selectCategories],
+    (categories) => categories.reduce((acc, category) => {
+        const { title, items } = category;
+        acc[title.toLowerCase()] = items;
+        return acc;
+    }, {})
+);
