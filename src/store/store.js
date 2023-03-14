77713aa8b55455bb27fa3ca0a21382d0132f9395
Emir Vranac <emir.vranac@symphony.is>
2023-03-14 14:44:34 +0100

    Create custom logger

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,23 @@
 import { compose, legacy_createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+//import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-const middlewares = [logger];
+const loggerMiddleware = (store) => (next) => (action) => {
+    if (!action.type) {
+        return next(action);
+    }
+
+    console.log('Type: ', action.type);
+    console.log('Payload: ', action.payload);
+    console.log('Current State: ', store.getState());
+
+    next(action);
+
+    console.log('Next state: ', store.getState());
+}
+
+const middlewares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
